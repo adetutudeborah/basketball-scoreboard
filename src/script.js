8a08090cde250dbab3bb57d29277888944746665
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-02-21 23:01:11 +0100

    added game badge

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
 let homeScore = document.getElementById("home");
-let guestScore = document.getElementById("guest")
+let guestScore = document.getElementById("guest");
+let homeTeamBadge = document.getElementsByClassName("home-team-badge");
+let guestTeamBadge = document.getElementsByClassName("guest-team-badge");
 
 let home = 0;
 let guest = 0;
 
-
+// Home Increment function
 function plusOne() {
    home += 1
    homeScore.textContent = home
@@ -20,6 +22,7 @@ function plusTwo() {
     homeScore.textContent = home 
  }
 
+ // Guest Increment function
  function scorePlusOne() {
     guest += 1
     guestScore.textContent = guest
@@ -35,6 +38,8 @@ function plusTwo() {
     guestScore.textContent = guest
   }
 
+  // Reset function 
+
   function resetScore(){
     home = 0;
     guest = 0;
@@ -42,3 +47,22 @@ function plusTwo() {
     guestScore.textContent = guest
 
   }
+
+  // Game Badge 
+
+function gameBadge(home, guest) {
+   if (home > guest) {
+      homeTeamBadge.textContent = "WINNER";
+      guestTeamBadge.style.display = none;
+
+   } else if (home < guest) {
+      guestTeamBadge.textContent = "WINNER";
+      homeTeamBadge.style.display = none;
+
+   } else {
+      homeTeamBadge.textContent = "DRAW";
+      guestTeamBadge.textContent = "DRAW";
+   }
+}
+
+gameBadge(home, guest);
